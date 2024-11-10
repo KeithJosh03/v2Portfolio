a9d873148766947164d45689d7c6ca13fbec1ada
KeithJosh03 <kitjuswa0304@gmail.com>
2024-11-10 23:57:47 +0800

    initializing add feature stack section

--- components/sections/stacks/stacks.tsx
+++ components/sections/stacks/stacks.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { poppins } from '@/app/utils'
 
+import { reactjs } from '@/public/skills'
 
 export const Stack = () => {
   return (
---
+++ public/skills/index.tsx
@@ -0,0 +1,23 @@
+import css  from './css.png';
+import html  from './html.png';
+import javascript  from './javascript.png';
+import jquery  from './jquery.png';
+import laravel  from './laravel.png';
+import nextjs  from './nextjs.png';
+import php  from './php.png';
+import reactjs  from './reactjs.png';
+import tailwind  from './tailwind.png';
+
+export {
+    css,
+    html,
+    javascript,
+    jquery,
+    laravel,
+    nextjs,
+    php,
+    reactjs,
+    tailwind
+}
+
+
